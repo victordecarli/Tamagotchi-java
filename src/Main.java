9cafeace3cb9f71e2ccd8b6bb2186f2940bae5cd
Victor de Carli <vonorvictor@gmail.com>
2025-11-11 22:17:00 -0400

    chore: Inicia projeto em java

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+void main() {
+    // cria um objeto atraves da Class chamado 'Cachorro'
+    Animal pluto = new Cachorro();
+    pluto.tipo = "Cachorro";
+    pluto.energia = 100;
+    pluto.higiene = 100;
+    pluto.saude = 100;
+    pluto.felicidade = 100;
+
+    Animal pirata = new Papagaio();
+    pirata.tipo = "Animal";
+    pirata.energia = 100;
+    pirata.higiene = 100;
+    pirata.saude = 100;
+    pirata.felicidade = 100;
+
+    Animal garfield = new Gato();
+    garfield.tipo = "Animal";
+    garfield.energia = 100;
+    garfield.higiene = 100;
+    garfield.saude = 100;
+    garfield.felicidade = 100;
+
+}
